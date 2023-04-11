6b766d68c8757f9d39abb7e0029ab674368802c3
Sergei Pankov <sergeipankov98@mail.ru>
2023-04-11 10:25:44 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-// Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const app = express();
 
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {});
 
